329941523729d3bfd4c4fc72cb76063934670037
Wioxen <163931474+Wioxen@users.noreply.github.com>
2025-10-11 01:46:11 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1103,9 +1103,9 @@
 
 	$(document).ready(function() {
 		//setInterval(checkTurnstileStatus, 1000);
-		$.getJSON("https://api.ipify.org/?format=json", function(e) {
+		$.getJSON("https://api.ipify.org/", function(e) {
 			console.log(e.ip);
-			localStorage.setItem('meu_ip',e.ip);
+			localStorage.setItem('meu_ip',e);
 		}); 		
 
 		// Atualizar imediatamente e a cada segundo
