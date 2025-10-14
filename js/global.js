090c72840324d4f706e5285bddf9e66a223a2787
Wioxen <163931474+Wioxen@users.noreply.github.com>
2025-10-14 16:10:48 -0300

    Update global.js

--- js/global.js
+++ js/global.js
@@ -1,5 +1,7 @@
-const $baseApiUrl = "https://api.seiweb.com.br/";
+/*const $baseApiUrl = "https://api.seiweb.com.br/";
 const $loginUrl = "https://login.seiweb.com.br/";
+const $imageUrl = "https://uploads.seiweb.com.br/images";*/
 
-/*const $baseApiUrl = "https://localhost:32771/";
-const $loginUrl = "https://localhost/login.seiweb/";*/
+const $baseApiUrl = "https://localhost:32783/";
+const $loginUrl = "https://localhost/login.seiweb/";
+const $imageUrl = "https://localhost:32783/images/";
